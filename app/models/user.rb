2490db4ad3194b3eca91f07681ffccba7e1b6a15
elgnonvis <elgnonvis@gmail.com>
2021-10-04 13:57:29 +0100

    Adding validations for User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+    validates :email, presence: true,
+    uniqueness: { case_sensitive: false},
+    length: {maximum: 255},
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, length: {minimum: 8}
+    before_validation { email.downcase! }
+    
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_122220) do
+ActiveRecord::Schema.define(version: 2021_10_04_125015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_10_04_122220) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
